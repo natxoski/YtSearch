4ad0c23e2594b137bac06156de3a764ec890d2e1
Ignacio Garcia Villanueva <ignacio.gvillanueva@gmail.com>
2018-02-07 22:56:36 +1100

    initial

---
+++ index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = '';
+
+
+
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={ 
+            videos: [],
+            selectedVideo: null 
+        };
+
+        this.videoSearch('surfboards');
+        
+    }
+    
+    videoSearch(term) {
+        YTSearch({key:API_KEY, term: term},(videos) => {
+            this.setState({
+                videos : videos, 
+                selectedVideo: videos[0]
+             });
+            //console.log(videos);
+            //this.setState({videos:videos});
+        });
+    }
+
+ render(){
+        const videoSearch = _.debounce( term =>{ this.videoSearch(term)}, 300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange = {videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container')); 
+
